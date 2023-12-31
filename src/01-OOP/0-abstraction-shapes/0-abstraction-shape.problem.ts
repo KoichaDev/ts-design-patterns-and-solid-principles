e7ba4bab3190db4563c73338b5ca9057d438dd59
Khoi Hoang <khoii87@gmail.com>
2023-12-31 01:38:36 +0100

    add: ✨ abstraction of shape example

---
+++ src/01-OOP/0-abstraction-shapes/0-abstraction-shape.problem.ts
@@ -0,0 +1,16 @@
+/**
+ * Create a program to have multiple shapes, which includes a circle, a rectangle
+ * and the responsibility of our program is to calculate the area of the shapes
+ * as well as the perimeter of the shape, despite of the shape that we provide to it.
+ * It can be a circle, and a rectangle to start off with, but later on, we can have
+ * more shapes like square etc.
+ *
+ * In the end, we want to provide a simple interface, or a single function to our user
+ * or our developers, where they can use this single function, such as e.g. we want to
+ * create a function called calculateTotalArea(). This function will be exposed
+ * to the area of the shape, and they will not be exposed to any other thing.
+ * We will not expose to a circle class, or the the rectangle class, or to the shape class
+ * Our responsibility here is to provide them with a single function, which is called
+ * calculate total area, and this function should be able to calculate the area of any shape
+ * that is provided to this particular function
+ */
---
+++ src/01-OOP/0-abstraction-shapes/0-abstraction-shape.solution.ts
@@ -0,0 +1,41 @@
+type Shape = {
+	area(): number;
+	perimeter(): number;
+};
+
+class Circle implements Shape {
+	constructor(private radius: number) {}
+
+	area(): number {
+		return Math.PI * this.radius * this.radius;
+	}
+
+	perimeter(): number {
+		return 2 * Math.PI * this.radius;
+	}
+}
+
+class Rectangle implements Shape {
+	constructor(private width: number, private height: number) {}
+
+	area(): number {
+		return this.width * this.height;
+	}
+
+	perimeter(): number {
+		return 2 * (this.width + this.height);
+	}
+}
+
+function calculateTotalArea(shape: Shape): number {
+	return shape.area();
+}
+
+// client code - this is what they need to use enough amount of the code,
+// and the class of the shapes above itself is abstracted away, and shouldn't be visible to the user
+
+const circle: Circle = new Circle(5);
+const rectangle: Rectangle = new Rectangle(4, 6);
+
+console.log('Area of circle: ', calculateTotalArea(circle));
+console.log('Area of rectangle: ', calculateTotalArea(rectangle));
