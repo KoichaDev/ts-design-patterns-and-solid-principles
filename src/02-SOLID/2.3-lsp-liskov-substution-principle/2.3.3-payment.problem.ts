0ffde31151fb4b159f8bea69cb24b54c7774016e
Khoi Hoang <khoii87@gmail.com>
2024-01-09 20:51:06 +0100

    add: ✨ Payment problem and solution for LSP

---
+++ src/02-SOLID/2.3-lsp-liskov-substution-principle/2.3.3-payment.problem.ts
@@ -0,0 +1,7 @@
+/**
+ * Let us assume we are going to work with an e-commerce application, and this application has payment processor class
+ * This payment processor class is responsible for processing 3 different types of payments, which is
+ * Credit Card, Debit Card and PayPal. Use the Liskov Principle to create the implementation of this kind of structure
+ * where we have the payment processor central class, and then there are three different types of methods that can be used
+ * in order to process the payment
+ */
