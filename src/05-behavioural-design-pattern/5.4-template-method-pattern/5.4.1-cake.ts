a259b0b35d2f102b4f9dd6650c8e16a83c6febe2
Khoi Hoang <khoii87@gmail.comm>
2024-03-09 13:28:33 +0100

    add: ✨ bake cake template method pattern

---
+++ src/05-behavioural-design-pattern/5.4-template-method-pattern/5.4.1-cake.ts
@@ -0,0 +1,59 @@
+(() => {
+	abstract class CakeRecipe {
+		public bakeCake(): void {
+			this.preHeatOven();
+			this.mixIngredients();
+			this.bake();
+			this.coolingDown();
+			this.decorate();
+		}
+
+		public bro() {
+			console.log('brooo!!!');
+		}
+
+		protected preHeatOven(): void {
+			console.log('Preheat oven to 175 degrees Celsius');
+		}
+
+		protected bake(): void {
+			console.log('Baking cake...');
+		}
+
+		protected coolingDown(): void {
+			console.log('Cooling down the cake...');
+		}
+		protected decorate(): void {
+			console.log('Decorating the vanilla cake...');
+		}
+
+		protected abstract mixIngredients(): void;
+	}
+
+	class ChocolateCake extends CakeRecipe {
+		protected mixIngredients(): void {
+			console.log('Mixing: Chocolate, sugar, butter, flour and eggs');
+		}
+
+		protected decorate(): void {
+			console.log('Decorating the chocolate cake with chocolate chips');
+		}
+	}
+
+	class VanillaCake extends CakeRecipe {
+		protected mixIngredients(): void {
+			console.log('Mixing: Vanilla extract, sugar, butter, flour and eggs');
+		}
+	}
+
+	// Client Code
+	function bakeCake(cake: CakeRecipe) {
+		cake.bakeCake();
+	}
+
+	console.log('Baking a Chocolate Cake');
+	bakeCake(new ChocolateCake());
+
+	console.log('Baking a Vanilla Cake');
+	bakeCake(new VanillaCake());
+})();
